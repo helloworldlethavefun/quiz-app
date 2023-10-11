b29032288c8f6bcaa2bb348fefe4417d675e0ced
BlueFloyd12321 <bluefloyd12321@gmail.com>
2023-10-11 20:20:05 +0000

    comit

--- simple-quiz/main.py
+++ simple-quiz/main.py
@@ -1,62 +1,13 @@
-# This is the simple version of the quiz. It's a basic cli with a little bit of file/db handling
-
-# All of the imports
-import cs50
-import platform
-import os
-import sys
+# This is the really simple quiz
 
 # Variables
-# This function defines some global variables for the program to use
 
-def varis():
-    global score
-    global name
-    global questions_used
-    global user_os
-    score = 0
-    name = ""
-    questions_used = []
-    user_os = ""
+score = 0
+question_number = 0
+name = ""
 
-# Functions
+# The main code
 
-# This is just a quick function to clear the users screen
-def clear():
-    user_os = platform.system()
-    if user_os == "Darwin" or "Linux":
-        os.system("clear")
-    elif user_os == "Windows":
-        os.system("cls")
-    else:
-        print("Sorry there was an error trying to clear your screen")
-        sys.exit(2)
+name = input("What is your name? ")
 
-# The "Intro" to the the program
-def start():
-    clear()
-    
-    name = input("What is your name? ")
-
-    clear()
-
-    print("------------------------------------------------------")
-    print("     Wecome to the ultimate gaming quiz", name, "     ")
-    print("------------------------------------------------------")
-
-# This creates the connection to the sql-lite db
-def setup_sql_conn():
-    pass
-
-# This pulls the question from the sql db
-def fetch_question():
-    pass
-
-# Main function
-
-def main():
-    start()
-
-if __name__ == "__main__":
-    varis()
-    main()
+print("--------------")
