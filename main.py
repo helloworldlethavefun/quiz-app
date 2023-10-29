0bd20cbcba0f372eb5a69bde61faf8d006c50e18
BlueFloyd12321 <bluefloyd12321@gmail.com>
2023-10-29 20:34:25 +0000

    Just doing fixups :D

--- main.py
+++ main.py
@@ -3,24 +3,15 @@
 import sys
 import time
 
-def clearscreen():
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
 # Asks for name 
-def askforname():
-    name = input("What is your name? ")
-    return name
+name = input("What is your name? ")
 
-# clear the screen and ask for the users name
-clearscreen()
-name = askforname()
-
-clearscreen()
-
+# Welcome the user to the program and ask if they would like to play the quiz
 print(f"Welcome to the ultimate gaming quiz {name}!")
+print("Would you like to play a game? ")
 print("Enter no to quit")
-playing = input("Would you like to play a game? ")
-
+playing = input("> ")
 
 if playing.lower() != "yes":
     quit()
@@ -32,7 +23,6 @@ while True:
     time.sleep(1)
     score = 0
 
-    clearscreen()
 
     QUESTION = input("In Minecraft, what material is commonly used to create torches? \na) Iron \nb) Gold \nc) Coal \nd) Diamond\n> ")
 
@@ -40,13 +30,11 @@ while True:
         print("\nCORRECT! \nNext Question")
         score += 1
         time.sleep(1)
-        clearscreen()
 
         
     if QUESTION.upper() != "C":
         print("\nARE YOU EVEN A REAL GAMER?? ")
         score += 1
-        clearscreen()
 
 
     QUESTION = input("Who of the following are shield operators? \na) Monty \nb) Caveria \nc) Recruit \nd) Rook \n>  ")
@@ -55,12 +43,10 @@ while True:
         print("\nYOU ARE QUITE A GENIUS HEY?")
         score += 1
         time.sleep(1)
-        clearscreen()
     
     if QUESTION.upper() != "D":
         print("\nNOPE, GO PLAY SOME MORE SEIGE MATE!")
         time.sleep(1)
-        clearscreen()
     
     QUESTION = input("\nWhat item is required to summon the Eye of Cthulhu in Terraria? \na) Demonite Ore \nb) Suspicious Looking Eye \nc) Moon Charm \nd) Shadow Scale \n> ")
     
@@ -68,12 +54,10 @@ while True:
         print("\nGOOD JOB! YOU KNOW YOUR STUFF")
         score += 1
         time.sleep(1)
-        clearscreen()
     
     if QUESTION.capitalize() != "B":
         print("\nHEY GO PLAY SOME TERRARIA AND THEN COME BACK AND GET THIS RIGHT!")
         time.sleep(1)
-        clearscreen()
 
     
     QUESTION = input("\nWhich character in Overwatch wields a rocket launcher and a jetpack? \na) Tracer \nb) Soldier: 76 \nc) Pharah \nd) Mei \n> ")
@@ -82,12 +66,10 @@ while True:
         print("\nHEY! IT'S SOMEONE ELSE WHO HAS PLAYED OVERWATCH!")
         score += 1
         time.sleep(1)
-        clearscreen()
     
     if QUESTION.capitalize() != "C":
         print("\nI'M HIGHLY DOUBTING YOUR GAMING KNOWLEDGE RIGHT NOW HERE DUDE!")
         time.sleep(1)
-        clearscreen()
 
     QUESTION = input("\nIn Fallout 4, what is the name of the robotic companion created by the Institute? \na) Curie \nb) Codsworth \nc) Dogmeat \nd) Strong \n> ")
     
@@ -95,14 +77,12 @@ while True:
         print("\nNICE YOU'VE PLAYED FALLOUT BEFORE!")
         score += 1
         time.sleep(1)
-        clearscreen()
     
     if QUESTION.capitalize() != "B":
         print("\nOH COME ON, YOU DON'T LIKE POST NUCLEAR GAMES?!")
         time.sleep(1)
-        clearscreen()
     
-    total_percent = score / 5 * 100
+    total_percent = (score / 100) * 100
 
     print(f"You scored: {total_percent} on this quiz!")
     again = input("Would you like to play again? ")
